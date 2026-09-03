bef5db58b0b51fd2a78c89618bacb68301b0af24
con-mag <mohamed.ramadan.con3@gmail.com>
2026-09-03 17:35:26 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = Object.freeze({
-  // اكتب عنوان API الخاص بك بعد نشر مجلد api.
-  API_BASE: "https://workspace-xhig-neon.vercel.app/api",
-  POLL_MS: 15000,
-  APP_NAME: "المركز"
-});
